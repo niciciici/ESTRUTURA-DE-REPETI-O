cc01efa01dcea43b83176d1792a612cdf055aaed
niciciici <nicolascarvalho140108@gmail.com>
2024-06-27 16:23:21 -0300

    Add files via upload

---
+++ estruturaderepetição/03.php
@@ -0,0 +1,11 @@
+<?php 
+
+$n = 10;
+$impar;
+for($i = 0; $i < $n ; $i++){
+    $impar = (2 * $i + 1);
+    echo "{$impar}\n";    
+
+
+}
+?>
---
+++ estruturaderepetição/04.php
@@ -0,0 +1,10 @@
+<?php 
+$n = 30;
+$multiplos;
+
+for ($i = 0; $i < $n; $i++){
+$multiplos = $i * 3;
+echo "{$multiplos}\n";
+
+}
+?>
